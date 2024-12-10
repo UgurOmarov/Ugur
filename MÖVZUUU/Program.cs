1f80e3c235b4c94366f196e2af7ad1404efd0acd
UgurOmarov <uguromarov888@gmail.com>
2024-12-10 16:58:17 +0400

    init

---
+++ MÖVZUUU/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MÖVZUUU
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
